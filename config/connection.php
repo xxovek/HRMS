d050e16a6350cdbb6fe4aef3882698bc02bf5a3f
vbpawar <vikaspawar3110@gmail.com>
2019-05-14 16:00:05 +0530

    changes

--- config/connection.php
+++ config/connection.php
@@ -3,6 +3,6 @@ $servername = 'localhost';
 $username   = 'root';
 $password   = '';
 $dbname ='RecentHrms';
-$con = new mysqli($servername,$username,$password,$dbname) or die(mysqli_error());
+$con = new mysqli($servername,$username,$password,$dbname) or die(mysqli_error($con));
 ?>
 
--- index.php
+++ index.php
@@ -3,9 +3,9 @@ error_reporting(E_ALL);
    session_start();
    if(!isset($_SESSION['a_id'])){
       
-         header('Location:/Public_html/login.php');
+         header('Location:Public_html/login.php');
    }
   else{
-       header('Location:/Public_html/index.php');
+       header('Location:Public_html/index.php');
   }
 ?>
